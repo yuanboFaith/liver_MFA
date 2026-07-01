e189b8ed761d7ccc1f9fea78e6dac333208c7350
Bo Yuan <yuanbo.faith@gmail.com>
2026-07-01 11:15:04 -0400

    refed TAG kinetics portal AAs CI computed for most of them

--- LvM_Refed_TAGkinetics_portalAAs/6_4_confidence_sumFlux7.R
+++ LvM_Refed_TAGkinetics_portalAAs/6_4_confidence_sumFlux7.R
@@ -589,13 +589,13 @@ func.calculate_confidence_interval <- function(f){
       
       
       
-      # # Other fatty acids srcAcCoA should be about 1.2 times higher than the total linoleate oxidation flux (extrapolated from the fasting state)
-      # i.TAGLino  <- func.findIndex("TAGLino.Lv->AcCoA.Lv")
-      # i.Lino     <- func.findIndex("Lino.Blood->AcCoA.Lv")
-      # i.srcAcCoA <- func.findIndex("srcAcCoA->AcCoA.Lv")
-      # 
-      # Amat <- rbind( Amat,    Amat0[i.srcAcCoA, ] - 1.2 * (Amat0[i.Lino, ] +  Amat0[i.TAGLino, ]  ) )
-      # bo   <- c(bo, 0)
+      # Other fatty acids srcAcCoA should be about 1.2 times higher than the total linoleate oxidation flux (extrapolated from the fasting state)
+      i.TAGLino  <- func.findIndex("TAGLino.Lv->AcCoA.Lv")
+      i.Lino     <- func.findIndex("Lino.Blood->AcCoA.Lv")
+      i.srcAcCoA <- func.findIndex("srcAcCoA->AcCoA.Lv")
+
+      Amat <- rbind( Amat,    Amat0[i.srcAcCoA, ] - 1.2 * (Amat0[i.Lino, ] +  Amat0[i.TAGLino, ]  ) )
+      bo   <- c(bo, 0)
       # 
       # Amat <- rbind( Amat, -c(Amat0[i.srcAcCoA, ] - 1.2 * (Amat0[i.Lino, ] +  Amat0[i.TAGLino, ]) ))
       # bo   <- c(bo, -1)
